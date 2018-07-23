c9c381bef93fa5debab4b03eb8d8be0c8bafd082
Moussa <mbaradji221@outlook.fr>
2018-07-23 17:03:57 +0200

    first

---
+++ index.vue
@@ -0,0 +1,84 @@
+<script>
+import Vue from "vue";
+import contextManager from "./contextManager.vue";
+const ComponentCtor1 = Vue.extend(contextManager);
+const ClassName = "contextStudio";
+const globalType = typeof window === "undefined" ? global : window;
+const spinalSystem = globalType.spinal.spinalSystem;
+
+const PanelTitle1 = "Context Manager";
+const ButtonLabel1 = "Context Manager";
+const ButtonIcon1 = "bookmarks";
+
+const classExtention = class {
+  constructor(viewer, options) {
+    Autodesk.Viewing.Extension.call(this, viewer, options);
+    this.viewer = viewer;
+    this.panel1 = null;
+  }
+  load() {
+    if (this.viewer.toolbar) {
+      this.createUI();
+    } else {
+      this.onToolbarCreatedBinded = this.onToolbarCreated.bind(this);
+      this.viewer.addEventListener(
+        av.TOOLBAR_CREATED_EVENT,
+        this.onToolbarCreatedBinded
+      );
+    }
+
+    return true;
+  }
+  onToolbarCreated() {
+    this.viewer.removeEventListener(
+      av.TOOLBAR_CREATED_EVENT,
+      this.onToolbarCreatedBinded
+    );
+    this.onToolbarCreatedBinded = null;
+    this.createUI();
+  }
+  unload() {
+    this.viewer.toolbar.removeControl(this.subToolbar);
+    return true;
+  }
+  // This function is to create your button on viewer, it used autodesk forge api
+  createUI() {
+    this.panel1 = new PanelClass(this.viewer, PanelTitle1);
+    globalType.spinal.panelManager.registerPanel(this.panel1, "contextManager");
+
+    var button1 = new Autodesk.Viewing.UI.Button(ButtonLabel1);
+    globalType.spinal.panelManager.registerButton(button1, "contextManager");
+    button1.container.style.color = "red";
+    var icon = button1.container.firstChild;
+    icon.className = "adsk-button-icon md-icon md-icon-font md-theme-default";
+    icon.innerHTML = ButtonIcon1;
+    button1.setToolTip(ButtonLabel1);
+
+    this.initialize();
+  }
+  initialize() {
+    var _container1 = document.createElement("div");
+    _container2.className = this.panel2.container.id + "-pannelcontainer";
+    _container2.style.height = "calc(200% - 45px)";
+    _container2.style.overflowY = "auto";
+    this.panel2.container.appendChild(_container2);
+    new ComponentCtor2().$mount(_container2);
+  }
+};
+export default new class {
+  constructor() {
+    Autodesk.Viewing.theExtensionManager.registerExtension(
+      ClassName,
+      classExtention
+    ); // this is the register of your class
+    window.spinal.ForgeExtentionManager.addExtention(ClassName);
+  }
+}();
+</script>
+
+
+<style>
+.bkcolor {
+  background-color: rgba(17, 48, 223, 0.644);
+}
+</style>
